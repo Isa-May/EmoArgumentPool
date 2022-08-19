ee8f2a79db5dcebfa1068eb37b4c88641e1ed94f
MyCodeIsland <39490763+MyCodeIsland@users.noreply.github.com>
2022-08-19 19:06:06 +0200

    succesfully added data to database from fixture (still problem with id though)

---
+++ data_preprocessing.py
@@ -0,0 +1,33 @@
+import pandas
+
+df = pandas.read_csv('emotional_arguments.csv')
+
+df.drop('confidence', inplace=True, axis=1)
+df.drop('dataset', inplace=True, axis=1)
+
+df.drop('stance', inplace=True, axis=1)
+
+df['model']='simpleDataViewer.EmoArguments'
+df = df.reindex(['model', 'id', 'argument', 'arg_strength', 'topic', 'label'], axis=1)
+
+df = df.to_json('emotional_arguments.json', orient = 'records')
+
+#df.drop('Id', inplace=True, axis=1)
+#df = df.iloc[: , 1:]
+#df.to_csv('emotional_arguments.csv', index=False)
+from django.db import transaction
+
+
+##df = pandas.read_csv('emotional_arguments.csv')
+#argument_ = df['argument']
+#arg_strength_ = df['arg_strength']
+#topic_ = df['topic']
+#stance_ = df['stance']
+#label_ = df['label']
+
+#all_attributes = zip(argument_, arg_strength_, topic_,stance_, label_)
+#with transaction.atomic():
+ #   for obj_ in all_attributes:
+  #      .save()
+
+#print(list(all_attributes))
--- simpleDataViewer/models.py
+++ simpleDataViewer/models.py
@@ -1,12 +1,13 @@
+import pandas
 from django.db import models
 
 # Create your models here.
 
 class EmoArguments(models.Model):
-    id = models.PositiveIntegerField(primary_key=True)
     argument = models.TextField()
     arg_strength = models.FloatField()
     topic = models.TextField()
-    stance = models.PositiveIntegerField()
+   # stance = models.PositiveIntegerField()
     label = models.PositiveIntegerField()
+
 
