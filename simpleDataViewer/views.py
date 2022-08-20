cd1ec34bffd018b569c7dc31f859691997e6a0c0
MyCodeIsland <39490763+MyCodeIsland@users.noreply.github.com>
2022-08-20 17:07:40 +0200

    created method that returns all arguments more orderly

--- simpleDataViewer/views.py
+++ simpleDataViewer/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import EmoArgument
 
 
 # Create your views here.
 # This is a request handler; it takes a request and returns a response
 from django.template import loader
-from .models import EmoArgument
+from django.shortcuts import render
+
 
 def say_hello(request):
     my_args=EmoArgument.objects.all().values()
-    output = ""
-    for x in my_args:
-        output += x["argument"]
-    return HttpResponse(output)
+    template = loader.get_template('hello.html')
+    context = {
+        'my_args': my_args,
+    }
+ #   output = ""
+  #  for x in my_args:
+   #     output += x["argument"]
+    return HttpResponse(template.render(context, request))
     #return render(request, 'hello.html')
     #  template = loader.get_template('hello.html')
     #  return HttpResponse(template.render())
