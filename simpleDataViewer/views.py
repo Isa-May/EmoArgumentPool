b04eb2344b15412d784e3d18ed13331c20ac8300
MyCodeIsland <39490763+MyCodeIsland@users.noreply.github.com>
2022-08-20 16:53:05 +0200

    created method that returns all arguments

--- simpleDataViewer/views.py
+++ simpleDataViewer/views.py
@@ -4,10 +4,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 # This is a request handler; it takes a request and returns a response
+from django.template import loader
+from .models import EmoArgument
 
 def say_hello(request):
+    my_args=EmoArgument.objects.all().values()
+    output = ""
+    for x in my_args:
+        output += x["argument"]
+    return HttpResponse(output)
     #return render(request, 'hello.html')
-    return HttpResponse('Hello World')
+    #  template = loader.get_template('hello.html')
+    #  return HttpResponse(template.render())
+    #return HttpResponse('Hello World')
 
 #def sort_by(request):
  #   return HttpResponse('')
