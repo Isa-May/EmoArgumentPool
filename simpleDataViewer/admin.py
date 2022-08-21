f9cb66ffa5e262fcaac30b3875117713819e955f
MyCodeIsland <39490763+MyCodeIsland@users.noreply.github.com>
2022-08-21 09:45:02 +0200

    Used TestCase instead of SimpleTestCase

--- simpleDataViewer/admin.py
+++ simpleDataViewer/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import EmoArgument
+admin.site.register(EmoArgument)
 # Register your models here.
--- simpleDataViewer/tests.py
+++ simpleDataViewer/tests.py
@@ -5,7 +5,11 @@ from django.test import SimpleTestCase
 from django.urls import reverse
 
 
-class HomepageTests(SimpleTestCase):
+class HomepageTests(TestCase):
     def test_is_url_accessible_via_name(self):
         response = self.client.get(reverse("render_arguments"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_if_url_location_ok(self):
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
