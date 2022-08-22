e29924193809930644672d03079287deb28b4bf8
MyCodeIsland <39490763+MyCodeIsland@users.noreply.github.com>
2022-08-22 12:52:07 +0200

    deleted frontend

--- SimplePythonAPI/settings.py
+++ SimplePythonAPI/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'simpleDataViewer', 
-    'rest_framework'
-]
+    'simpleDataViewer']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
