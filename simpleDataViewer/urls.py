30d1d12a985f3400d90c04bfee6b36d4a3c736e6
MyCodeIsland <39490763+MyCodeIsland@users.noreply.github.com>
2022-08-27 17:26:41 +0200

    new urls

--- simpleDataViewer/urls.py
+++ simpleDataViewer/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('render_arguments_by_topic_1/', views.render_arguments_by_topic_1, name="render_arguments_by_topic_1"),
     path('render_arguments_by_topic_2/', views.render_arguments_by_topic_2, name="render_arguments_by_topic_2"),
     path('render_arguments_by_topic_3/', views.render_arguments_by_topic_3, name="render_arguments_by_topic_3"),
+    path('render_arguments_by_topic_4/', views.render_arguments_by_topic_4, name="render_arguments_by_topic_4"),
+    path('render_arguments_by_topic_5/', views.render_arguments_by_topic_5, name="render_arguments_by_topic_5"),
+    path('render_arguments_by_topic_6/', views.render_arguments_by_topic_6, name="render_arguments_by_topic_6"),
+
 ]
--- simpleDataViewer/views.py
+++ simpleDataViewer/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 
 def render_arguments(request):
     my_args=EmoArgument.objects.all().values()
-    template = loader.get_template('render_arguments.html')
+    template = loader.get_template('render_arguments_emo.html')
     context = {
         'my_args': my_args,
     }
@@ -12,38 +12,54 @@ def render_arguments(request):
 
 
 def render_arguments_by_topic_1(request):
-    my_args = EmoArgument.objects.all().filter(topic='tv-is-better-than-books')
-    template = loader.get_template('render_arguments.html')
+    my_args = EmoArgument.objects.all().filter(topic='tv-is-better-than-books', label=1)
+    template = loader.get_template('render_arguments_emo.html')
     context = {
         'my_args': my_args,
     }
     return HttpResponse(template.render(context, request))
 
 def render_arguments_by_topic_2(request):
-    my_args = EmoArgument.objects.all().filter(topic='firefox-vs-internet-explorer')
-    template = loader.get_template('render_arguments.html')
+    my_args = EmoArgument.objects.all().filter(topic='firefox-vs-internet-explorer', label=1)
+    template = loader.get_template('render_arguments_emo.html')
     context = {
         'my_args': my_args,
     }
     return HttpResponse(template.render(context, request))
 
 def render_arguments_by_topic_3(request):
-    my_args = EmoArgument.objects.all().filter(topic='ban-plastic-water-bottles')
-    template = loader.get_template('render_arguments.html')
+    my_args = EmoArgument.objects.all().filter(topic='ban-plastic-water-bottles', label=1)
+    template = loader.get_template('render_arguments_emo.html')
     context = {
         'my_args': my_args,
     }
     return HttpResponse(template.render(context, request))
 
-#render arguments on certain topics
-### emotional or non-emotional
-### pro or contra
+def render_arguments_by_topic_4(request):
+    my_args = EmoArgument.objects.all().filter(topic='if-your-spouse-committed-murder-and-he-or-she-confided-in-you-would-you-turn-them-in-', label=1)
+    template = loader.get_template('render_arguments_emo.html')
+    context = {
+        'my_args': my_args,
+    }
+    return HttpResponse(template.render(context, request))
+
+def render_arguments_by_topic_5(request):
+    my_args = EmoArgument.objects.all().filter(topic='pro-choice-vs-pro-life', label=1)
+    template = loader.get_template('render_arguments_emo.html')
+    context = {
+        'my_args': my_args,
+    }
+    return HttpResponse(template.render(context, request))
 
 
-    # if contains school-uniform in topic
-    # tv-is-better-than-books
-    # firefox-vs-internet-explorer
-    # ban-plastic-water-bottles
+def render_arguments_by_topic_6(request):
+    my_args = EmoArgument.objects.all().filter(topic='evolution-vs-creation', label=1)
+    template = loader.get_template('render_arguments_emo.html')
+    context = {
+        'my_args': my_args,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def render_welcome_page(request):
     template = loader.get_template('welcome.html')
